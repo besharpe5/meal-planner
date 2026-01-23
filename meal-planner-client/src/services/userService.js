a0300224cc75513c678d10e565b86750dbc83ec5
besharpe5 <besharpe15@gmail.com>
2026-01-23 11:05:39 -0500

    created userService.js file

---
+++ meal-planner-client/src/services/userService.js
@@ -0,0 +1,16 @@
+import API from "./api";
+
+export async function getMe() {
+  const res = await API.get("/user/me");
+  return res.data;
+}
+
+export async function updateEmail(email, currentPassword) {
+  const res = await API.put("/user/email", { email, currentPassword });
+  return res.data;
+}
+
+export async function updatePassword(currentPassword, newPassword) {
+  const res = await API.put("/user/password", { currentPassword, newPassword });
+  return res.data;
+}
