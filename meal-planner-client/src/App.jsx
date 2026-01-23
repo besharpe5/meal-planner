9b55b73810e5ec862a6a5ea1412afda5a2dd950e
besharpe5 <besharpe15@gmail.com>
2026-01-23 11:42:48 -0500

    profile page is fully functional

--- meal-planner-client/src/App.jsx
+++ meal-planner-client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 import Navbar from "./components/Navbar";
 
