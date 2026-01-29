812f3097c28db2de337cdda4ccd5e730bd45c683
besharpe5 <besharpe15@gmail.com>
2026-01-29 13:59:00 -0500

    updated api interceptor

--- meal-planner-client/src/services/api.js
+++ meal-planner-client/src/services/api.js
@@ -10,10 +10,14 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
-    config.headers = config.headers || {};
+    config.headers = config.headers ?? {};
     config.headers.Authorization = `Bearer ${token}`;
+  } else {
+    // prevent stale header
+    if (config.headers) delete config.headers.Authorization;
   }
   return config;
 });
+
 
 export default api;
