ba29052eab43f50cdf52ba8cdf43d62e6b457ad3
besharpe5 <besharpe15@gmail.com>
2026-01-30 17:04:28 -0500

    debugging Vercel failure

--- meal-planner-client/pages/+config.js
+++ meal-planner-client/pages/+config.js
@@ -2,5 +2,5 @@ import vikeReact from "vike-react/config";
 
 export default {
   extends: [vikeReact],
-  prerender: true, // generates real HTML at build-time for pages (SSG)
+  prerender: ["/"], // ✅ prerender only the landing page
 };
