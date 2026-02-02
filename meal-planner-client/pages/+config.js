9ee703ecfcd6b9a8f2fdf5f4334311f37ef912c3
besharpe5 <besharpe15@gmail.com>
2026-02-01 22:31:39 -0500

    updated vercel.json

--- meal-planner-client/pages/+config.js
+++ meal-planner-client/pages/+config.js
@@ -2,5 +2,5 @@ import vikeReact from "vike-react/config";
 
 export default {
   extends: [vikeReact],
-  prerender: ["/", "/about"], // ✅ prerender only the landing page
+  prerender: ["/", "/about", "/app"], // ✅ prerender only the landing page
 };
